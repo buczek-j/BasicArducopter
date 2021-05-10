3c031fcab76790b8bff9aec9ba19fc49453e3b10
buczek-j <buczekjohn@gmail.com>
2021-05-10 15:44:50 -0400

    changed to LLA only; updated file pathing; modified examples

--- tools/CommonStructs.py
+++ tools/CommonStructs.py
@@ -9,6 +9,10 @@ class Frames(enum.Enum):
     NED = 'NED'
     VEL = 'VEL'
 
+
+R = 6378137.0  # Equator radius in meters
+
+
 def arc_to_deg(arc):
     """convert spherical arc length [m] to great circle distance [deg]"""
     return float(arc)/6371/1000 * 180/math.pi
@@ -31,63 +35,53 @@ def latlon_to_xyz(lat,lon):
     z = r * math.cos(theta)
     return [x,y,z]
 
-def xyz_to_latlon (x,y,z):
-    """Convert cartesian to angular lat/lon coordiantes"""
-    r = math.sqrt(x**2 + y**2 + z**2)
-    theta = math.asin(z/r) # https://stackoverflow.com/a/1185413/4933053
-    phi = math.atan2(y,x)
-    lat = math.degrees(theta)
-    lon = math.degrees(phi)
-    return [lat,lon]
+def xyz_to_latlon (ref, dn, de, dd):
+    """
+    Convert cartesian to angular lat/lon coordiantes
+    :param ref: Waypoint object for reference origin
+    :param dn: float meters north
+    :param de: float meters east
+    :param dd: float meters down
+    :return: Waypoint class object of the desired position in LLA
+    """
+    dLat = dn/R
+    dLon = de/(R*math.cos(math.pi*ref.lat/180))
+
+    lat = ref.lat + (dLat*180/math.pi)
+    lon = ref.lon + (dLon*180/math.pi)
+    alt = ref.alt - dd
+
+    return Waypoint(x=lat, y=lon, z=alt)
 
 #######################################
 ### Waypoint Object
 #######################################
 
 class Waypoint():
-    def __init__(self, frame=Frames.LLA, x=0.0, y=0.0, z=0.0, compass_angle=0.0, duration=0.0, name=None):
+    def __init__(self, x=0.0, y=0.0, z=0.0, compass_angle=0.0, name=None):
         '''
         Waypoint Class
-        :param frame: Frame of reference of the waypoint
-        :param x: float, lat for LLA, dNorth for NED
-        :param y: float, long for LLA, dEast for NED
-        :param z: alt msl for LLA, dDown for NED
-        :param compass_angle: float for  the desired yaw angle (0 = North, 90 = east)
-        :param duration: time to hold position at waypoint (seconds)
+        :param x: float, lat for LLA
+        :param y: float, long for LLA
+        :param z: alt msl for LLA
+        :param compass_angle: float for  the desired yaw angle (0 = North, pi/2 = east)
         :param name: string of the name of the waypoint
-        :param ekf_offset: array of floatf for dNorth (m) and dEast (m) offset from the EKF origin to normalize multiple vehicles' local coordinate frames
         '''
-        self.frame = frame          # Frame of reference 
         self.name = name            # Name, currently unused
         self.phi = compass_angle    # Desired compass angle at waypoint (0 = North, 90 = East) default: 0
-        self.duration = duration    
 
-        if frame == Frames.LLA:     # Latitude, Longitude, Altitude
-            self.lat = x
-            self.lon = y
-            self.alt = z
-        
-        elif frame == Frames.NED:   # dNorth, dEast, dDown
-            self.dNorth = x 
-            self.dEast = y 
-            self.dDown = z
-            
+        self.lat = x
+        self.lon = y
+        self.alt = z
 
     def update (self, vehicle):
         '''
         Method will update the waypoint with the current vehicle location
         :param vehicle: vehicle dronekit object
         '''
-		
-        if self.frame == Frames.LLA:
-            self.lat = vehicle.location.global_frame.lat
-            self.lon = vehicle.location.global_frame.lon
-            self.alt = vehicle.location.global_frame.alt	
-        
-        elif self.frame == Frames.NED:
-            self.dNorth = vehicle.location.local_frame.north 
-            self.dEast = vehicle.location.local_frame.east 
-            self.dDown = vehicle.location.local_frame.down
+        self.lat = vehicle.location.global_frame.lat
+        self.lon = vehicle.location.global_frame.lon
+        self.alt = vehicle.location.global_frame.alt	
     
     def current_distance(self, vehicle, to_waypoint=None):
         '''
@@ -96,48 +90,36 @@ class Waypoint():
         :param to_waypoint: is a Waypoint object. If inputted, vehicle is ignored
         :return: the current distance in meters from the vehicle to the waypoint or an inputted waypoint
         '''
-        if self.frame == Frames.LLA:
 
-            if to_waypoint == None: # If no waypoint is specified, give distance from vehicle
-                dlat = self.lat - vehicle.location.global_frame.lat
-                dlong = self.lon - vehicle.location.global_frame.lon
-                dalt = self.alt - vehicle.location.global_frame.alt
-                phi_2=math.radians(vehicle.location.global_frame.lat)
-            else:
-                dlat = self.lat - to_waypoint.lat
-                dlong = self.lon - to_waypoint.lon
-                dalt = self.alt - to_waypoint.alt
-                phi_2=math.radians(to_waypoint.lat)
-            
-            R=6371000                               # radius of Earth in meters
-            phi_1=math.radians(self.lat)
-            
-
-            delta_phi=math.radians(dlat)
-            delta_lambda=math.radians(dlong)
-
-            a=math.sin(delta_phi/2.0)**2+\
-                math.cos(phi_1)*math.cos(phi_2)*\
-                math.sin(delta_lambda/2.0)**2
-
-            c=2*math.atan2(math.sqrt(a),math.sqrt(1-a))
-
-            ground_dist = R*c 
-
-            dist = math.sqrt((ground_dist * ground_dist) + (dalt * dalt))   # add altitude distance
-            return dist                        # output distance in meters
+        if to_waypoint == None: # If no waypoint is specified, give distance from vehicle
+            dlat = self.lat - vehicle.location.global_frame.lat
+            dlong = self.lon - vehicle.location.global_frame.lon
+            dalt = self.alt - vehicle.location.global_frame.alt
+            phi_2=math.radians(vehicle.location.global_frame.lat)
+        else:
+            dlat = self.lat - to_waypoint.lat
+            dlong = self.lon - to_waypoint.lon
+            dalt = self.alt - to_waypoint.alt
+            phi_2=math.radians(to_waypoint.lat)
         
-        elif self.frame == Frames.NED:
-            if to_waypoint == None:
-                delta_north = self.dNorth - vehicle.location.local_frame.north 
-                delta_east = self.dEast - vehicle.location.local_frame.east 
-                delta_down = self.dDown - vehicle.location.local_frame.down
-            else:
-                delta_north = self.dNorth - to_waypoint.dNorth 
-                delta_east = self.dEast - to_waypoint.dEast
-                delta_down = self.dDown - to_waypoint.dDown
-            dist = math.sqrt((delta_north*delta_north) + (delta_east*delta_east + (delta_down*delta_down)))
-            return dist
+        #R=6371000                               # radius of Earth in meters
+        phi_1=math.radians(self.lat)
+        
+
+        delta_phi=math.radians(dlat)
+        delta_lambda=math.radians(dlong)
+
+        a=math.sin(delta_phi/2.0)**2+\
+            math.cos(phi_1)*math.cos(phi_2)*\
+            math.sin(delta_lambda/2.0)**2
+
+        c=2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+
+        ground_dist = R*c 
+
+        dist = math.sqrt((ground_dist * ground_dist) + (dalt * dalt))   # add altitude distance
+        return dist                        # output distance in meters
+        
 
     def current_bearing(self, vehicle, to_waypoint=None):
         '''
@@ -171,7 +153,6 @@ class Waypoint():
         :param to_waypoint: is a Waypoint object. If inputted, vehicle is ignored
         :return: array of floats [dNorth, dEast] in meters
         '''
-        R = 6378137.0  # Equator radius in meters
         f = 0.00335281066474748071  # 1/298.257223563, inverse flattening
 
         if to_waypoint==None:   # if no waypoint specified, then give vehicle bearing
@@ -204,7 +185,4 @@ class Waypoint():
         return [dNorth, dEast]
 
     def print(self):
-        if self.frame == Frames.LLA:
-            print("lat : {} lon: {} alt: {} phi: {}".format(self.lat, self.lon, self.alt, self.phi))
-        elif self.frame == Frames.NED:
-            print("dNorth : {} dEast: {} dDown: {} phi: {}".format(self.dNorth, self.dEast, self.dDown, self.phi))
+        print("lat : {} lon: {} alt: {} phi: {}".format(self.lat, self.lon, self.alt, self.phi))
