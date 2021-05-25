3e699c73456cce488a21fa83c0de705e1b76b69e
buczek-j <buczekjohn@gmail.com>
2021-05-25 09:23:53 -0400

    made test code

--- power_test.py
+++ power_test.py
@@ -4,23 +4,26 @@ from time import sleep, time
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--connection_string', type=str, default='/dev/ttyTHS1', help='Ardupilot connection string')
+    parser.add_argument('--connection_string', type=str, default='/dev/ttyACM0', help='Ardupilot connection string')
     options = parser.parse_args()
 
     # simple use example
-    drone = BasicArdu(connection_string=options.connection_string)    # connect to ArduPilot
+    drone = BasicArdu(connection_string=options.connection_string, tolerance_location=1)    # connect to ArduPilot
 
     # takeoff drone
     drone.handle_takeoff(5)   
     sleep(3)
     start_time = time()
-    while drone.vehicle.battery.voltage > 21.6:
-    	# goto first waypoint (6m north, 0 meters east, 5 meters up, facing North)
-    	drone.handle_waypoint(Frames.NED, 6, 0, -5, 0)
+    while drone.vehicle.battery.voltage > 28.8:
+    	# goto first waypoint (5m north, 0 meters east, 5 meters up, facing North)
+    	drone.handle_waypoint(Frames.NED, 5, 0, -5, 0)
     	sleep(3)
 	
-    	# goto second wayoint(0m north, 5 meters east, 5 meters up, facing South)
-    	drone.handle_waypoint(Frames.NED, 0, 5, -5, 3.14)
+    	# goto second wayoint(5m north, 5 meters east, 5 meters up, facing North)
+    	drone.handle_waypoint(Frames.NED, 5, 5, -5, 0)
+    	sleep(3)
+
+        drone.handle_waypoint(Frames.NED, 0, 5, -5, 3.14)
     	sleep(3)
 	
     	# goto Home wayoint (0m north, 0 meters east, 5 meters up, facing North)
